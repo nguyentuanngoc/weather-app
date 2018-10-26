dc550b0b8bc0b1787977ebd8d05cc0e06f305a3b
Ngoc <ngocnguyen94nk@gmail.com>
2018-10-26 21:15:10 +0700

    Updates

---
+++ precache-manifest.c7c198923b0c92baa3da5e54ba5ce72b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2be6f7590b391fa0dcfc",
+    "url": "/weather-app/static/js/runtime~main.2be6f759.js"
+  },
+  {
+    "revision": "3381e60a3ce8e7ad61e7",
+    "url": "/weather-app/static/js/main.3381e60a.chunk.js"
+  },
+  {
+    "revision": "88bff4650d1c453bc1d8",
+    "url": "/weather-app/static/js/1.88bff465.chunk.js"
+  },
+  {
+    "revision": "3381e60a3ce8e7ad61e7",
+    "url": "/weather-app/static/css/main.eb5bff4c.chunk.css"
+  },
+  {
+    "revision": "e71b1daa9e0b384c6524d62b952452a0",
+    "url": "/weather-app/index.html"
+  }
+];
